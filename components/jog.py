01cb9904f9b2da3cb060c68a16f8043e94f81509
Stefano Bertelli <bartei81@gmail.com>
2022-10-22 00:17:29 -0500

    feat: add tabs for modes

---
+++ components/jog.py
@@ -0,0 +1,30 @@
+import logging
+import os
+import time
+import collections
+
+from decimal import Decimal
+
+from kivy.clock import Clock
+from kivy.lang import Builder
+from kivy.properties import StringProperty, ConfigParserProperty, NumericProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App
+from components.appsettings import config
+from typing import Callable
+
+
+log = logging.getLogger(__file__)
+kv_file = os.path.join(os.path.dirname(__file__), __file__.replace(".py", ".kv"))
+if os.path.exists(kv_file):
+    log.info(f"Loading KV file: {kv_file}")
+    Builder.load_file(kv_file)
+
+
+class LabelButton(BoxLayout):
+    release_function: Callable
+    pass
+
+
+class Jog(BoxLayout):
+    pass
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import logging
 import os
 
 from kivy.app import App
 from kivy.clock import Clock
-from kivy.properties import StringProperty, NumericProperty, ConfigParserProperty
+from kivy.properties import StringProperty, NumericProperty, ConfigParserProperty, BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
 
 from components.appsettings import config
 from utils import communication
+
+log = logging.getLogger(__file__)
 
 
 class Home(BoxLayout):
@@ -56,6 +59,9 @@ class MainApp(App):
     y_axis = NumericProperty(20)
     z_axis = NumericProperty(20)
 
+    in_motion = BooleanProperty(False)
+    motion_enable = BooleanProperty(False)  # Normal = False, down = True
+
     desired_position = NumericProperty(0.0)
     current_position = NumericProperty(0.0)
 
@@ -63,7 +69,13 @@ class MainApp(App):
     division_index = NumericProperty(0)
     division_offset = NumericProperty(0.0)
 
+    jog_speed = NumericProperty(0.1)
+    jog_accel = NumericProperty(0.01)
+    jog_forward = BooleanProperty(False)
+    jog_backward = BooleanProperty(False)
+
     # device = communication.DeviceManager()
+    home = None
 
     def set_current_position(self, value):
         self.current_position = value
@@ -80,24 +92,43 @@ class MainApp(App):
     def set_divisions(self, value):
         self.divisions = value
 
+    def set_jog_speed(self, value):
+        self.jog_speed = value
+
+    def set_jog_accel(self, value):
+        self.jog_accel = value
+
     def update_desired_position(self, *args, **kwargs):
+        if not self.divisions > 0:
+            self.divisions = 1
+
         self.desired_position = 360 / self.divisions * self.division_index + self.division_offset
         self.division_index = self.division_index % self.divisions
         return True
 
     def update(self, *args, **kwargs):
-        self.current_position = self.device.get_current_position() / 400 * 360
+        if (abs(self.current_position - self.desired_position) > 0.1) and self.motion_enable:
+            self.in_motion = True
+        else:
+            self.in_motion = False
 
-    # def on_desired_position(self, instance, value):
-    #     self.device.set_final_position(int(value / 360 * 400))
+        print(self.jog_backward, self.jog_forward)
+
+    def on_desired_position(self, instance, value):
+        pass
+        # self.device.set_final_position(value)
+
+    def on_motion_enable(self, instance, value):
+        log.info("Motion enable status: {}".format(self.motion_enable))
+        pass
 
     def build(self):
-        home = Home()
+        self.home = Home()
         self.bind(divisions=self.update_desired_position)
         self.bind(division_index=self.update_desired_position)
         self.bind(division_offset=self.update_desired_position)
-        # Clock.schedule_interval(self.update, 1.0 / 10)
-        return home
+        Clock.schedule_interval(self.update, 1.0 / 10)
+        return self.home
 
 
 if __name__ == '__main__':
