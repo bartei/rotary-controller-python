050f3cec19c88549648eb9ffed45a4eae03c7583
Stefano Bertelli <stefano@provvedo.com>
2022-12-31 17:48:42 -0600

    fix: improve read scales performance

--- utils/communication.py
+++ utils/communication.py
@@ -1,3 +1,6 @@
+import struct
+from typing import List
+
 import minimalmodbus
 import logging
 
@@ -8,8 +11,8 @@ REG_MODE = 0
 REG_CURRENT_POSITION = 2
 REG_FINAL_POSITION = 4
 REG_UNUSED_6 = 6
-REG_ENCODER_POSITION = 8
-REG_UNUSED_10 = 10
+REG_UNUSED_8 = 8
+REG_ENCODER_PRESET_INDEX = 10
 REG_ENCODER_PRESET_VALUE = 12
 REG_UNUSED_14 = 14
 REG_MAX_SPEED = 16
@@ -21,6 +24,14 @@ REG_RATIO_DEN = 26
 REG_UNUSED_28 = 28
 REG_SYN_RATIO_NUM = 30
 REG_SYN_RATIO_DEN = 32
+REG_SYN_OFFSET = 34
+REG_SYN_SCALE_INDEX = 36
+REG_SCALE_1 = 38
+# REG_SCALE_2 = 40
+# REG_SCALE_3 = 42
+# REG_SCALE_4 = 44
+
+SCALES_COUNT = 4
 
 # Summary of the currently available device modes from the current firmware
 MODE_HALT = 0
@@ -56,6 +67,7 @@ class DeviceManager:
         except Exception as e:
             self.connected = False
             self.last_error = e.__str__()
+            print(self.last_error)
             return 0
 
     @mode.setter
@@ -68,20 +80,28 @@ class DeviceManager:
             self.last_error = e.__str__()
 
     @property
-    def x_position(self):
-        try:
-            value = self.device.read_long(
-                REG_ENCODER_POSITION,
-                byteorder=minimalmodbus.BYTEORDER_LITTLE_SWAP,
-                signed=True
-            )
-            self.connected = True
-            return value
-        except Exception as e:
-            self.connected = False
-            self.last_error = e.__str__()
-            return 0
+    def scales(self) -> List[int]:
+            result = []
+            try:
+                raw_data = self.device.read_registers(REG_SCALE_1, SCALES_COUNT * 2)
+                self.connected = True
+            except Exception as e:
+                self.connected = False
+                self.last_error = e.__str__()
+                result.append(0)
+                return [0, 0, 0, 0]
 
+            raw_byte = bytearray()
+            format = ">"
+            for i in range(SCALES_COUNT):
+                raw_byte.append((raw_data[i*2+1] >> 8) & 255)
+                raw_byte.append((raw_data[i*2+1]) & 255)
+                raw_byte.append((raw_data[i*2] >> 8) & 255)
+                raw_byte.append((raw_data[i*2]) & 255)
+                format += "l"
+
+            result = struct.unpack(format, raw_byte)
+            return result
 
     @property
     def current_position(self):
@@ -400,6 +420,33 @@ class DeviceManager:
                 value,
                 signed=True,
                 byteorder=minimalmodbus.BYTEORDER_LITTLE_SWAP
+            )
+            self.connected = True
+        except Exception as e:
+            self.connected = False
+            self.last_error = e.__str__()
+
+    @property
+    def encoder_preset_index(self):
+        try:
+            value = self.device.read_register(
+                REG_ENCODER_PRESET_INDEX,
+                signed=False
+            )
+            self.connected = True
+            return value
+        except Exception as e:
+            self.connected = False
+            self.last_error = e.__str__()
+            return 0
+
+    @encoder_preset_index.setter
+    def encoder_preset_index(self, value):
+        try:
+            self.device.write_register(
+                REG_ENCODER_PRESET_INDEX,
+                value,
+                signed=False
             )
             self.connected = True
         except Exception as e:
