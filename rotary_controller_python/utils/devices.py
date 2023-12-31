785aa9c2ccabda391922f6e9174c0adc2244ac65
Stefano Bertelli <stefano@provvedo.com>
2023-12-31 12:10:40 -0600

    fix: normalize syn ratio den for scales

--- rotary_controller_python/utils/devices.py
+++ rotary_controller_python/utils/devices.py
@@ -186,7 +186,7 @@ class Scale(BaseDevice):
 
     @ratio_den.setter
     def ratio_den(self, value):
-        self.write_long(self.addresses.ratio_den, value) * 1000
+        self.write_long(self.addresses.ratio_den, value)
 
     @property
     def max_value(self):
