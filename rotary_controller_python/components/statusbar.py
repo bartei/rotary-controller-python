193e05e10265c1f573203868e3114453fbd6940d
Stefano Bertelli <stefano@provvedo.com>
2024-03-13 18:43:02 -0500

    fix: update logging for statusbar

--- rotary_controller_python/components/statusbar.py
+++ rotary_controller_python/components/statusbar.py
@@ -1,10 +1,11 @@
 import os
 
+from kivy.logger import Logger
 from kivy.properties import NumericProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
-from loguru import logger as log
 
+log = Logger.getChild(__name__)
 kv_file = os.path.join(os.path.dirname(__file__), __file__.replace(".py", ".kv"))
 
 if os.path.exists(kv_file):
