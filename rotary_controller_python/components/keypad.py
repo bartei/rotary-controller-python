14fb684e80b1bd0cbea5181c6584a6ddc3b181a6
Stefano Bertelli <stefano@provvedo.com>
2024-03-13 18:21:29 -0500

    feat: update logging for keypad

--- rotary_controller_python/components/keypad.py
+++ rotary_controller_python/components/keypad.py
@@ -1,8 +1,9 @@
 import os
-
-from loguru import logger as log
+from kivy.logger import Logger
 from kivy.uix.popup import Popup
 from kivy.lang import Builder
+
+log = Logger.getChild(__name__)
 
 kv_file = os.path.join(os.path.dirname(__file__), __file__.replace(".py", ".kv"))
 if os.path.exists(kv_file):
