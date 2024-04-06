a3111b5afd0cdf713a35807dc979edf22befb1a3
Stefano Bertelli <stefano@provvedo.com>
2024-04-06 17:23:14 -0500

    fix: update servobar with new interface to device

--- rotary_controller_python/components/servobar.py
+++ rotary_controller_python/components/servobar.py
@@ -7,7 +7,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
 
 from rotary_controller_python.dispatchers import SavingDispatcher
-from rotary_controller_python.utils.communication import DeviceManager
+from rotary_controller_python.utils.devices import Global
 
 log = Logger.getChild(__name__)
 
@@ -32,7 +32,7 @@ class ServoBar(BoxLayout, SavingDispatcher):
     current_position = NumericProperty(0.0)
     desired_position = NumericProperty(0.0)
 
-    def __init__(self, device: DeviceManager, **kv):
+    def __init__(self, device: Global, **kv):
         self.device = device
         super().__init__(**kv)
         self.upload()
@@ -40,42 +40,39 @@ class ServoBar(BoxLayout, SavingDispatcher):
     def upload(self):
         props = self.get_our_properties()
         prop_names = [item.name for item in props]
-        device_props = self.get_writeable_properties(type(self.device.servo))
+
+        device_props = [item.name for item in self.device['servo'].variables]
+
         matches = [item for item in prop_names if item in device_props]
         for item in matches:
-            self.device.servo.__setattr__(item, self.__getattribute__(item))
+            self.device['servo'][item] = self.__getattribute__(item)
 
     def on_index(self, instance, value):
         if self.divisions != 0 and self.device is not None:
-            self.device.index.index = self.index
-            self.device.index.divisions = self.divisions
+            self.device['index']['index'] = self.index
+            self.device['index']['divisions'] = self.divisions
         else:
             log.error("Divisions must be != 0")
         return True
 
     def on_offset(self, instance, value):
-        self.device.servo.absolute_offset = self.offset
-        self.offset = self.device.servo.absolute_offset
+        self.device['servo']['absolute_offset'] = self.offset
+        self.offset = self.device['servo']['absolute_offset']
 
     def on_divisions(self, instance, value):
-        self.device.index.divisions = self.divisions
+        self.device['index']['divisions'] = self.divisions
 
     def on_min_speed(self, instance, value):
-        self.device.servo.min_speed = self.min_speed
+        self.device['servo']['min_speed'] = self.min_speed
 
     def on_max_speed(self, instance, value):
-        self.device.servo.max_speed = self.max_speed
+        self.device['servo']['max_speed'] = self.max_speed
 
     def on_acceleration(self, instance, value):
-        self.device.servo.acceleration = self.acceleration
+        self.device['servo']['acceleration'] = self.acceleration
 
     def on_ratio_num(self, instance, value):
-        self.device.servo.ratio_num = self.ratio_num
+        self.device['servo']['ratio_num'] = self.ratio_num
 
     def on_ratio_den(self, instance, value):
-        self.device.servo.ratio_den = self.ratio_den
-
-    def on_enable(self, instance, vlaue):
-        # todo: implement this in the c side
-        # self.device.servo.enable = self.enable
-        pass
+        self.device['servo']['ratio_den'] = self.ratio_den
