923854b43f53b92792dc47ebe5c703f4211f9703
Stefano Bertelli <stefano@provvedo.com>
2024-08-03 15:50:48 -0500

    fix: add handling of overflow with method for uint32 using ctypes

--- rotary_controller_python/utils/ctype_calc.py
+++ rotary_controller_python/utils/ctype_calc.py
@@ -2,4 +2,4 @@ import ctypes
 
 
 def uint32_subtract_to_int32(a, b):
-    return ctypes.c_int32(ctypes.c_uint32(a) - ctypes.c_uint32(b)).value
+    return ctypes.c_int32(ctypes.c_uint32(a).value - ctypes.c_uint32(b).value).value
