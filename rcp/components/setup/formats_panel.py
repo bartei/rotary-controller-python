afc69e5d33f449355caacc7aba2856330affe0fb
Greg Kopp <greg@gregkopp.com>
2025-06-28 22:57:18 -0400

    Add mute button to formats

--- rcp/components/setup/formats_panel.py
+++ rcp/components/setup/formats_panel.py
@@ -15,8 +15,26 @@ if os.path.exists(kv_file):
 
 class FormatsPanel(BoxLayout):
     formats = ObjectProperty()
+    stored_volume = None  # Store the volume level before muting
 
     def __init__(self, formats, **kv):
         self.formats = formats
         super().__init__(**kv)
         self.ids['grid_layout'].bind(minimum_height=self.ids['grid_layout'].setter('height'))
+        
+    def toggle_mute(self):
+        """Toggle between muted and unmuted states"""
+        if self.formats.volume > 0:
+            # We're unmuted, so store the current volume and mute
+            self.stored_volume = self.formats.volume
+            self.formats.volume = 0
+            self.ids.volume_slider.value = 0
+        else:
+            # We're muted, so restore the previous volume
+            if self.stored_volume is not None and self.stored_volume > 0:
+                self.formats.volume = self.stored_volume
+                self.ids.volume_slider.value = self.stored_volume
+            else:
+                # If we don't have a stored volume or it's 0, set to default
+                self.formats.volume = 0.2
+                self.ids.volume_slider.value = 0.2
