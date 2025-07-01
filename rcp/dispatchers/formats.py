f172954713310566ded5db6bb54b3480ec3763a5
Greg Kopp <greg@gregkopp.com>
2025-06-30 22:46:15 -0400

    Toggle scale of feed rate

--- rcp/dispatchers/formats.py
+++ rcp/dispatchers/formats.py
@@ -3,7 +3,7 @@ from fractions import Fraction
 from kivy.logger import Logger
 from kivy.properties import (
     NumericProperty,
-    StringProperty, ListProperty, ObjectProperty,
+    StringProperty, ListProperty, ObjectProperty, BooleanProperty,
 )
 
 from rcp.dispatchers import SavingDispatcher
@@ -12,7 +12,7 @@ log = Logger.getChild(__name__)
 
 
 class FormatsDispatcher(SavingDispatcher):
-    _force_save = ['display_color']
+    _force_save = ['display_color', 'metric_speed_mode', 'imperial_speed_mode']
     metric_position = StringProperty("{:+0.3f}")
     metric_speed = StringProperty("{:+0.3f}")
 
@@ -26,6 +26,15 @@ class FormatsDispatcher(SavingDispatcher):
     speed_format = StringProperty()
     position_format = StringProperty()
     factor = ObjectProperty(Fraction(1, 1))
+    
+    # New properties to track the speed display mode
+    # True = M/min, False = mm/min for metric
+    # True = ft/min, False = in/min for imperial
+    metric_speed_mode = BooleanProperty(True)
+    imperial_speed_mode = BooleanProperty(True)
+    
+    # Speed conversion factor: 1.0 for standard units, higher for smaller units
+    speed_conversion_factor = NumericProperty(1.0)
 
     display_color = ListProperty([1, 1, 1, 1])
     accept_color = ListProperty([0.2, 1, 0.2, 1])
@@ -40,13 +49,31 @@ class FormatsDispatcher(SavingDispatcher):
 
     def update_format(self, *args, **kv):
         if self.current_format == "MM":
-            self.speed_format = f"{self.metric_speed} M/min"
+            if self.metric_speed_mode:
+                self.speed_format = f"{self.metric_speed} M/min"
+                self.speed_conversion_factor = 1.0
+            else:
+                self.speed_format = f"{self.metric_speed} mm/min"
+                self.speed_conversion_factor = 1000.0
             self.position_format = self.metric_position
             self.factor = Fraction(1, 1)
         else:
-            self.speed_format = f"{self.imperial_speed} Ft/min"
+            if self.imperial_speed_mode:
+                self.speed_format = f"{self.imperial_speed} Ft/min"
+                self.speed_conversion_factor = 1.0
+            else:
+                self.speed_format = f"{self.imperial_speed} in/min"
+                self.speed_conversion_factor = 12.0
             self.position_format = self.imperial_position
             self.factor = Fraction(10, 254)
+            
+    def toggle_speed_mode(self):
+        """Toggle between different speed display modes"""
+        if self.current_format == "MM":
+            self.metric_speed_mode = not self.metric_speed_mode
+        else:
+            self.imperial_speed_mode = not self.imperial_speed_mode
+        self.update_format()
 
     def toggle(self, *_):
         if self.current_format == "MM":
