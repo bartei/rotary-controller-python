ba5d2734f66256f88ff8ec5e27ae650753860c71
Stefano Bertelli <stefano@provvedo.com>
2025-07-07 12:05:12 -0500

    fix: better size handling for fonts

--- rcp/components/toolbars/keypad_button.py
+++ rcp/components/toolbars/keypad_button.py
@@ -1,18 +1,22 @@
+import os
+
+from kivy.lang import Builder
+from kivy.logger import Logger
 from kivy.uix.button import Button
-from kivy.properties import NumericProperty, StringProperty
+from kivy.properties import NumericProperty
+
+log = Logger.getChild(__name__)
+
+
+kv_file = os.path.join(os.path.dirname(__file__), __file__.replace(".py", ".kv"))
+if os.path.exists(kv_file):
+    log.info(f"Loading KV file: {kv_file}")
+    Builder.load_file(kv_file)
 
 
 class KeypadButton(Button):
-
     return_value = NumericProperty(0)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.text_halign = "center"
-        self.font_style = "bold"
-        self.halign = "center"
-        self.background_color = [1, 1, 1, 1]
         self.font_name = "fonts/Manrope-Bold.ttf"
-
-    def on_height(self, instance, value):
-        self.font_size = value / 3
--- rcp/components/toolbars/keypad_icon_button.py
+++ rcp/components/toolbars/keypad_icon_button.py
@@ -1,16 +1,22 @@
+import os
+
+from kivy.lang import Builder
+from kivy.logger import Logger
 from kivy.uix.button import Button
 from kivy.properties import NumericProperty
+
+log = Logger.getChild(__name__)
+
+
+kv_file = os.path.join(os.path.dirname(__file__), __file__.replace(".py", ".kv"))
+if os.path.exists(kv_file):
+    log.info(f"Loading KV file: {kv_file}")
+    Builder.load_file(kv_file)
 
 class KeypadIconButton(Button):
     return_value = NumericProperty(0)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.text_halign = "center"
-        self.font_style = "bold"
-        self.halign = "center"
-        self.background_color = [1, 1, 1, 1]
         self.font_name = "fonts/Font Awesome 6 Free-Solid-900.otf"
 
-    def on_height(self, instance, value):
-        self.font_size = value / 3
