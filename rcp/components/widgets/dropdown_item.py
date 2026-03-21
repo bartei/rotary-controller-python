e0d2044afe5ddb2f428d0f1552ad17396bb7908d
Stefano Bertelli <stefano@provvedo.com>
2026-03-21 11:38:09 -0500

    fix: add backdrop to the dropdown widget. change buttons color to distinguish the options from the rest

--- rcp/components/widgets/dropdown_item.py
+++ rcp/components/widgets/dropdown_item.py
@@ -1,3 +1,4 @@
+from kivy.graphics import Color, Rectangle
 from kivy.logger import Logger
 from kivy.properties import StringProperty, ListProperty, ObjectProperty
 from rcp.components.popups.help_popup import HelpPopup  # noqa: F401
@@ -9,6 +10,8 @@ from rcp.utils.kv_loader import load_kv
 
 log = Logger.getChild(__name__)
 load_kv(__file__)
+
+DROPDOWN_OPTION_COLOR = [0.15, 0.3, 0.55, 1]
 
 
 class DropDownItem(BoxLayout):
@@ -22,8 +25,18 @@ class DropDownItem(BoxLayout):
     def __init__(self, **kv):
         super().__init__(**kv)
         self.dropdown = DropDown()
+        self.dropdown.container.padding = [4, 4, 4, 4]
+        self.dropdown.container.spacing = 2
+        with self.dropdown.canvas.before:
+            Color(0, 0, 0, 0.9)
+            self._bg_rect = Rectangle()
+        self.dropdown.bind(pos=self._update_bg, size=self._update_bg)
         self._options = []
         self.dropdown.bind(on_select=lambda instance, x: setattr(self, 'value', x))
+
+    def _update_bg(self, *args):
+        self._bg_rect.pos = self.dropdown.pos
+        self._bg_rect.size = self.dropdown.size
 
     def delete_all_dropdown_options(self):
         for item in self._options:
@@ -41,7 +54,10 @@ class DropDownItem(BoxLayout):
         font_size = app.formats.font_size if app else 24
 
         for item in self.options:
-            btn = Button(text=item, size_hint_y=None, height=60, font_size=font_size)
+            btn = Button(
+                text=item, size_hint_y=None, height=60,
+                font_size=font_size, background_color=DROPDOWN_OPTION_COLOR,
+            )
             btn.bind(on_release=lambda btn: self.dropdown.select(btn.text))
             self.dropdown.add_widget(btn)
             self._options.append(btn)
